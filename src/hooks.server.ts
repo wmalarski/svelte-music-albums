0f1f2e536413c23ef11db273b1af51fa6bbfdc13
Wojciech Malarski <w.malarski@gmail.com>
2023-10-09 21:58:51 +0200

    fixed adding reviews

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,18 @@
-import { AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_ISSUER } from '$env/static/private';
+import {
+	AUTH0_CLIENT_ID,
+	AUTH0_CLIENT_SECRET,
+	AUTH0_ISSUER,
+	AUTH_SECRET
+} from '$env/static/private';
+import prisma from '$lib/server/db/client';
 import Auth0 from '@auth/core/providers/auth0';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import { SvelteKitAuth } from '@auth/sveltekit';
 
-export const handle = SvelteKitAuth({
+export const handle = SvelteKitAuth(async () => ({
+	adapter: PrismaAdapter(prisma),
+	trustHost: true,
+	secret: AUTH_SECRET,
 	providers: [
 		Auth0({
 			clientId: AUTH0_CLIENT_ID,
@@ -20,4 +30,4 @@ export const handle = SvelteKitAuth({
 			return session;
 		}
 	}
-});
+}));
