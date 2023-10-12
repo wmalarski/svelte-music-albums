d2bccf8a18b02e74f389774adb1dafe3dcce67cb
Wojciech Malarski <w.malarski@gmail.com>
2023-10-12 22:35:40 +0200

    adds review remove

---
+++ src/lib/modules/reviews/ReviewRemove.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import AlertDialog from "$lib/components/AlertDialog/AlertDialog.svelte";
+	import type { FindReviewsResultItem } from "$lib/server/data/reviews";
+
+	export let review: FindReviewsResultItem;
+</script>
+
+<AlertDialog 
+  descriptionText="Remove review"
+  onConfirm={() => console.log("onConfirm", review)}
+  titleText="Remove"
+  triggerText="Remove"
+/>
