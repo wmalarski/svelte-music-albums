8366b5a04712a042d3088c87463c5ece34c92cea
Wojciech Malarski <w.malarski@gmail.com>
2023-11-27 23:51:24 +0100

    fixes layout

--- src/routes/(authorized)/+layout.svelte
+++ src/routes/(authorized)/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Footer from '$lib/modules/common/Footer.svelte';
 	import Header from '$lib/modules/common/Header.svelte';
+	import { twCx } from '$lib/utils/twCva';
+	import styles from './layout.module.css';
 </script>
 
-<div class="grid overflow-y-hidden h-screen min-h-screen">
+<div class={twCx("grid overflow-y-hidden h-screen min-h-screen", styles.grid)}>
 	<Header />
 
 	<main class="overflow-y-hidden">
